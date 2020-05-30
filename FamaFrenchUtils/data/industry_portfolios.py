93b56175d1b7f4762b81ca31d6f2ccc9f8ffc3bc
jason.mellone@gmail.com <jason.mellone@gmail.com>
2020-05-30 15:20:00 -0400

    done for value weighted. needs to be made more flexible for extracting the equal weight section.

--- FamaFrenchUtils/data/industry_portfolios.py
+++ FamaFrenchUtils/data/industry_portfolios.py
@@ -1,16 +1,36 @@
 import pandas as pd
 import requests
 import zipfile
-import tempfile
 import os
+import re
+import datetime
+import numpy as np
 
 
 def unzip(arg):
     zf = zipfile.ZipFile(arg)
-    return {name: zf.read(name) for name in zf.namelist()}
+    return [zf.read(name) for name in zf.namelist()]
 
 
-def industry_portfolio_csv(N_portfolios=49, cleanup=True):
+def extract_date(arg):
+    arg = re.findall(string=str(arg), pattern='[0-9]{6}')[0] + '01'
+    dt = pd.to_datetime(arg)
+
+    i = 32
+    next_dt = dt + datetime.timedelta(i)
+    while next_dt.month != dt.month:
+        next_dt = dt + datetime.timedelta(i)
+        i -= 1
+    return dt + datetime.timedelta(i + 1)
+
+
+def industry_portfolio_csv(N_portfolios=49, cleanup=True, weighting='value'):
+    """
+
+    :param N_portfolios:
+    :param cleanup:
+    :return:
+    """
     url = 'https://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp/{N}_Industry_Portfolios_CSV.zip'.format(
         N=N_portfolios)
 
@@ -29,7 +49,22 @@ def industry_portfolio_csv(N_portfolios=49, cleanup=True):
     if cleanup:
         os.unlink(fpath)
 
-    return uz.copy()
+    lines = str(uz[0]).split('\\r\\n')[11:]
+
+    lines = [l.split(',') for l in lines]
+
+    df = pd.DataFrame(lines[1:], columns=lines[0])
+
+    first_line_break = df[df.iloc[:, 0] == ''].index[0]
+
+    df = df.iloc[0:first_line_break, :]
+
+    df.index = df.iloc[:, 0].apply(extract_date)
+    del df['']
+    df = df.replace(' -99.99', np.nan)
+    df = df.astype(float)
+
+    return df/100.
 
 
 if __name__ == '__main__':
